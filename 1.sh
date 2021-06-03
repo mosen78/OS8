ce0f345d1aae0f9e4be855dcec9ad6661f36f719
mosen78 <77380347+mosen78@users.noreply.github.com>
2021-06-03 14:06:27 +0430

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,10 @@
+mkdir f1 
+chmod 397 f1
+chmod 440 f1
+mkdir file1
+chmod a=rx file1
+mkdir sample
+chmod g=w sample
+chmod r+x sample
+mkdir test
+chmod u+g test
---
+++ 2.sh
@@ -0,0 +1,6 @@
+mkdir f2
+chmod -c f2
+mkdir f3
+chmod -r f3
+mkdir f4
+chmod -f f4
